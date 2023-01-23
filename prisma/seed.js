ea684cb96cc8c4f7771aa8e913060bcd08ca516a
Mohammed Khrishi <67297080+MohammedKhrishi@users.noreply.github.com>
2023-01-23 04:14:01 +0200

    Create seed.js

---
+++ prisma/seed.js
@@ -0,0 +1,72 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const userData = [
+  {
+    name: 'Alex',
+    email: 'alex@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Join the Prisma Slack',
+          content: 'https://slack.prisma.io',
+          published: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Michael',
+    email: 'michael@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Follow Prisma on Twitter',
+          content: 'https://www.twitter.com/prisma',
+          published: true,
+          viewCount: 42,
+        },
+      ],
+    },
+  },
+  {
+    name: 'Jack',
+    email: 'jack@prisma.io',
+    posts: {
+      create: [
+        {
+          title: 'Ask a question about Prisma on GitHub',
+          content: 'https://www.github.com/prisma/prisma/discussions',
+          published: true,
+          viewCount: 128,
+        },
+        {
+          title: 'Prisma on YouTube',
+          content: 'https://pris.ly/youtube',
+        },
+      ],
+    },
+  },
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const value of userData) {
+    const user = await prisma.user.create({
+      data: value,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
