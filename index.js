7eff51515e830c3ed43d951f58721464f88b1261
Mohammed Khrishi <67297080+MohammedKhrishi@users.noreply.github.com>
2023-01-23 04:25:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,132 +1,146 @@
-import express, { urlencoded } from 'express';
-import { body, check, validationResult } from 'express-validator';
-import asyncHandler  from 'express-async-handler';
-// create express instance
-const app = express();
-const port=process.env.PORT || 3000;
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
 
-// extract json data .. has a in-built body-parser module in express.json();
-app.use(express.json());
-app.use(express.urlencoded({'extended':'true'}))
+const prisma = new PrismaClient()
+const app = express()
 
-// setting up a RESTful API for a mini project for nodejs courseWork organised by GazaSkyGeeks
+app.use(express.json())
 
+app.post(`/signup`, async (req, res) => {
+  const { name, email, posts } = req.body
 
-const tasks=[
-    {id:1,
+  const postData = posts
+    ? posts.map((post) => {
+        return { title: post.title, content: post.content || undefined }
+      })
+    : []
 
-     title:'first day course', 
-
-     description:"welocme to my first task",
-
-      isCompleted: true
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email,
+      posts: {
+        create: postData,
+      },
     },
-
-    {id:2,
-
-    title:'second day course',
-    
-    description:"welocme to my second task",
-    
-    isCompleted: true
-
-    },
-
-    {id:3,
-        
-    title:'third day course',
-    
-    description:"welocme to my third task", 
-    
-    isCompleted: true
-
-    }
-];
-
-app.get('/api/tasks', asyncHandler (async(req,res)=>{
-
-    try{
-        res.status(200).json(tasks)
-
-    } catch(error){
-
-    console.log(error.stack);
-    res.status(404);
-    res.json({message:"something went wrong"})
-    }
-}))
-
-app.get('/api/tasks/:id', async (req,res)=>{
-
-    try{
-        const {id} =await req.params;
-        const taskID= await tasks.find((element)=>element.id== req.params.id);
-        res.status(200).json(tasks[id-1])
-    } catch(error){
-    res.status(500);
-    res.json({message:"something went wrong"})
-    }
+  })
+  res.json(result)
 })
 
-app.post('/api/tasks/SubmitTask', check('title').exists().isLength({ min: 14}).withMessage('must be at least 14 chars long')
-.matches(/\d/).withMessage('must contain a number'),body('desciption').exists().isLength({min: 20}).withMessage("please write at least 20 characters"),
-async(request, response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).jsonp(errors.array());
-    }
-    // const{id,title,description,isCompleted}= await request.body;
-    try {const newTask= await {
-        id:request.body.id,
-        title:request.body.title,
-        description:request.body.description,
-        isCompleted:request.body.isCompleted
-    };
 
-    tasks.push(newTask);
-    response.status(201).json(tasks);
-} catch(error){
-    response.status(404).json({message:message.error});
-    }
-  });
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+  })
+  res.json(result)
+})
 
-  
-app.put('/put/tasks/:id', async (request, response) => {
-    const {id} =await req.params;
-    // retrieve the  parameters value
-    if (!tasks[id]){
-        response.status(404, 'The task is not found').send();
-    } else{
-        const updatedTaskTitle=request.body.title;
-        tasks[id].title = updatedTaskTitle;
-        response.status(204).json(updatedTaskTitle).send({message:"done!"});
-    }
-  });
-  
+app.put('/post/:id/views', async (req, res) => {
+  const { id } = req.params
+  try {
+    const post = await prisma.post.update({
+      where: { id: Number(id) },
+      data: {
+        viewCount: {
+          increment: 1,
+        },
+      },
+    })
+    res.json(post)
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` })
+  }
+})
 
-app.delete('/delete/tasks/:id', async (request, response) => {
-    const {id} = await request.params;
-    if(tasks.filter(task => task.id == id).length !== 0){
-      tasks = tasks.filter(task => task.id !== id);
-      response.status(200).json({message:"task has been deleted successfully"});
-    }else{
-      response.status(404).json({message:"something went wrong"});
-    }
-  });
+app.put('/publish/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const postData = await prisma.post.findUnique({
+      where: { id: Number(id) },
+      select: {
+        published: true,
+      },
+    })
 
-app.patch('/patch/tasks/:id', async (request, response) => {
-    try {
-     const {id} = await req.params;
-     const taskToUpdate= await tasks.find((element)=>element.id== req.params.id);
-     const taskIndex=indexOf(taskToUpdate);
-     const taskObjectUpdated=Object.assign(taskToUpdate,req.body)
-     tasks[taskIndex]=taskToUpdate;
-    response.status(200).send(tasks);
-    } catch (error) {
-      response.status(404).send({message:"something went wrong"});
-    }
-  });
+    const updatedPost = await prisma.post.update({
+      where: { id: Number(id) || undefined },
+      data: { published: !postData.published || undefined },
+    })
+    res.json(updatedPost)
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` })
+  }
+})
 
-app.listen(port, () =>
-  console.log(`🚀 Server ready at: http://localhost:${port}`)
-);
+app.delete(`/post/:id`, async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.delete({
+    where: {
+      id: Number(id),
+    },
+  })
+  res.json(post)
+})
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+app.get('/user/:id/drafts', async (req, res) => {
+  const { id } = req.params
+  const drafts = await prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    })
+    .posts({
+      where: { published: false },
+    })
+
+  res.json(drafts)
+})
+
+app.get(`/post/:id`, async (req, res) => {
+  const { id } = req.params
+
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  })
+  res.json(post)
+})
+
+app.get('/feed', async (req, res) => {
+  const { searchString, skip, take, orderBy } = req.query
+  const or = searchString
+    ? {
+        OR: [
+          { title: { contains: searchString } },
+          { content: { contains: searchString } },
+        ],
+      }
+    : {}
+
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+      ...or,
+    },
+    include: { author: true },
+    take: Number(take) || undefined,
+    skip: Number(skip) || undefined,
+    orderBy: {
+      updatedAt: orderBy || undefined,
+    },
+  })
+
+  res.json(posts)
+})
+
+const server = app.listen(3000, () =>
+  console.log(`Server ready at: http://localhost:3000)
